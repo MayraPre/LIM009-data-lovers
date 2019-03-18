d30995a5d925532a8459ae26d975212b2a6c9705
DianaOropeza <diana94karina@hotmail.com>
2019-03-18 10:39:36 -0500

    Intentando filtrar y desbloqueando data

--- src/data.js
+++ src/data.js
@@ -8,3 +8,27 @@ const example = () => {
 };
 
 window.example = example;
+
+/*const idPokemon = (object)=>{
+   
+  var array2d=[];
+  var properties =Object.keys(object);
+  for(var i=0; i<properties.length;i++){
+    array2d.push([properties[0],object[properties[0]]]);
+  }
+  return array2d;
+}
+console.log(idPokemon(POKEMON));*/
+
+function filterPokemon(arrayPokemones,paramFilter){
+  let arrFiltrado=[];
+  arrayPokemones.forEach(element=>{
+    if (element.type.includes(paramFilter)){
+      arrFiltrado.push(element)
+    }
+  })
+} console.log(POKEMON.filterPokemon=filterPokemon);
+
+const namePokemon=()=>{
+  
+}
