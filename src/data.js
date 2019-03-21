d3460d646767458407396af56bbde562584d2088
DianaOropeza <diana94karina@hotmail.com>
2019-03-21 11:13:46 -0500

    Imrprime en pantalla nombre, tipo, ect

--- src/data.js
+++ src/data.js
@@ -9,38 +9,3 @@ const example = () => {
 
 window.example = example;
 
-/*const idPokemon =()=>{
-  let nom=[];
-  for(var i=0; i<POKEMON.length;i++){
-    console.log(i);
-    nom.push( );
-  }
-<<<<<<< HEAD
-  return 'idPokemon';
-};
-console.log(POKEMON.idPokemon=idPokemon);
-*/
-=======
-  return array2d;
-}
-console.log(idPokemon(POKEMON));*/
-
-function filterPokemon(arrayPokemones,paramFilter){
-  let arrFiltrado=[];
-  arrayPokemones.forEach(element=>{
-    if (element.type.includes(paramFilter)){
-      arrFiltrado.push(element)
-
-      
-    }
-  })
-} console.log(POKEMON.filterPokemon=filterPokemon);
->>>>>>> 4feeb8424663fed10d74abdf122a6bc537c4051a
-
-/*const idPokemon =()=>{
-  let id=[];
-  for(var i=0; i<POKEMON.pokemon.length;i++){
-    console.log(id);
-  }return id;
-}
-POKEMON.idPokemon=idPokemon;*/
--- src/main.js
+++ src/main.js
@@ -2,12 +2,22 @@
 
 const totalPokemons = POKEMON.pokemon;
 
-const a= POKEMON.pokemon[0];
-for(let i=0; i<totalPokemons.length;i++){
-    const e=totalPokemons[0];
-    console.log(e.name);
-}
+/* lista de nombres
+for(let x=0; x<2;x++){
+  console.log(totalPokemons[x].name);
+}*/
 
+/*listado de nombres de la A-Z
+const arr=[];
+for(let x=0; x<totalPokemons.length;x++){
+  arr.push(totalPokemons[x].name);
+}
+console.log(arr.sort());*/
+
+/*for(let i=0; i<totalPokemons.length;i++){
+    const e=totalPokemons[0];
+    console.log(e.next_evolution);
+}*/
 
 /*function filtrar(a){
     return a.type
@@ -15,52 +25,31 @@ for(let i=0; i<totalPokemons.length;i++){
 const result = totalPokemons.filter(filtrar);
 console.log(result[0].type);*/
 
-/*function tipo(tipo){
-    return `
-    <h4> Tipo </h4>
-    <ul class="tipo">
-    ${tipo.map((tipo)=>`<li>${tipo}</li>`).join('')}
-    </ul>
-    `
+
+const arrname=[];
+for(let x=0; x<totalPokemons.length;x++){
+  arrname.push(totalPokemons[x].next_evolution);
 }
+//console.log(arrname);
 
-function pokeTemplate(pok){
-    return `
-    <div class="Pokemon">
-    <img class="pok-img" src="${pok.img}">
-    <h2 class="pok-name">${pok.name}<h2>
-    ${pok.type ? tipo(pok.type) : ''}
-  </div>`
-}
+const a=arrname.forEach(function(element) {
+  console.log(element);
+});
 
-document.getElementById("app").innerHTML= 
-   `<h1 class="app-title"> Pokemons (${totalPokemons.length} results)</h1>
-   ${totalPokemons.map(pokeTemplate).join('')}
-   <p class="footer">These ${totalPokemons.length}</p>
-`
-*/
-
-/*const idPokemon = (totalPokemons)=>{
-    var arr=[];
-    var properties=Object.keys(totalPokemons);
-    for(var i=0; i<properties.length;i++){
-        arr.push(properties[0],totalPokemons[properties[0]]);
-        console.log(arr);
-}return arr;
-}
-
-/*
-for(var i=0; i<2; i++){
-   
-}return */
-
-/*const idPokemon = (POKEMON)=>{
-  var array2d=[];
-  var properties =Object.keys(POKEMON);
-  for(var i=0; i<properties.length;i++){
-    array2d.push([properties[0],POKEMON[properties[0]]]);
+const box= document.getElementById('app')
+const addPokemon=(pokemones)=>{
+  for(let i=0; i<pokemones.length;i++){
+    box.innerHTML+=`
+    <div>
+       <p>${pokemones[i].name}</p>
+       <figure>
+       <img src="${pokemones[i].img}" alt="Foto" class="img-perfil">
+       </figure>
+       <p>${pokemones[i].type}</p>
+       
+    </div>
+    `;
   }
-  return array2d;
 }
-console.log(array2d);*/
+addPokemon(totalPokemons);
 
